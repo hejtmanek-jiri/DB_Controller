bf379197643ca323fce8c49b6cfd5351d8b8d060
Jiří Hejtmánek, DiS <hejtmanek.jiri@gmail.com>
2023-10-16 16:36:51 +0200

    update

--- DB_Controller/Controllers/UpdateController.cs
+++ DB_Controller/Controllers/UpdateController.cs
@@ -1,11 +1,22 @@
 ﻿using DB_Controller.DbSettings;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using InfluxDB.Client.Api.Domain;
+using InfluxDB.Client;
+using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using DB_Controller.Models;
+using System.Globalization;
+using InfluxDB.Client.Writes;
+using InfluxDB.Client.Core;
 
 namespace DB_Controller.Controllers
 {
     public class UpdateController : DbController
     {
+        const int BATCH_SIZE = 5000;
         public UpdateController(IOptions<GeneralDbSettings> GeneralDbSettings, IOptions<InfluxDbSettings> influxDbSettings, IOptions<TimescaleDbSettings> TimescaleDbSettings) : base(GeneralDbSettings, influxDbSettings, TimescaleDbSettings)
         {
         }
@@ -23,7 +34,68 @@ namespace DB_Controller.Controllers
 
         public IActionResult UpdateDataInfluxDb()
         {
-            return Ok("Ok");
+            try { 
+                //zmenit cestu na CSV kde jsou data na update
+                uploadData("C:\\BC\\data\\data.csv");
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+
+            TempData["success"] = "Data successfully updated!";
+
+            return View("index");
+
+        }
+
+
+        private void uploadData(string path)
+        {
+            using var client = new InfluxDBClient("http://localhost:8086", _influxDbSettings.Token);
+
+            using var reader = new StreamReader("C:\\BC\\data\\data.csv");
+            using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+
+            var records = csv.GetRecords<Data>();
+
+            using var writeApi = client.GetWriteApi();
+            var batch = new List<PointData>();
+
+            foreach (var record in records)
+            {
+                var point = PointData.Measurement(record.Measurement)
+                                     .Tag("D1", record.D1)
+                                     .Tag("D2", record.D2)
+                                     .Tag("D3", record.D3)
+                                     .Tag("D4", record.D4)
+                                     .Tag("Author", record.Author)
+                                     .Field("Value", record.Value)
+                                     .Field("Corrected_value", record.Corrected_value)
+                                     .Timestamp(record.Timestamp, WritePrecision.S);
+                
+                batch.Add(point);
+
+                if (batch.Count >= 5000)
+                {
+                    writeApi.WritePoints(batch, _influxDbSettings.Bucket, _influxDbSettings.Org);
+                    batch.Clear();
+                }
+
+               
+            }
+            
+            if (batch.Count > 0)
+            {
+                try { 
+                    writeApi.WritePoints(batch, _influxDbSettings.Bucket, _influxDbSettings.Org);
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+            
 
         }
 
---
+++ DB_Controller/Models/Data.cs
@@ -0,0 +1,24 @@
+﻿using InfluxDB.Client.Core;
+
+namespace DB_Controller.Models
+{
+    public class Data
+    {
+        public string Measurement { get; set; }
+        public string D1 { get; set; }
+
+        public string D2 { get; set; }
+
+        public string D3 { get; set; }
+
+        public string D4 { get; set; }
+
+        public string Author { get; set; }
+
+        public double Value { get; set; }
+
+        public double Corrected_value { get; set; }
+        public long Timestamp { get; set; }
+
+    }
+}
