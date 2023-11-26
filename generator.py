10bc84fac6502a53f5ac6b59b669c3e4f5dd9028
Jiří Hejtmánek, DiS <52444294+hejtmanek-jiri@users.noreply.github.com>
2023-11-26 13:39:39 +0100

    timescale added

--- generator.py
+++ generator.py
@@ -1,17 +1,24 @@
+from datetime import datetime, timedelta
+import pytz
 import random
 import string
 
 
 hours_in_year = 8760
 
-
+#function for generating random strings
 @staticmethod 
 def random_string_generator(length): 
     return ''.join(random.choice(string.ascii_uppercase) for i in range(length))
 
+#Build date TIMESTAMPTZ for timescale
+timestamp_timescale = datetime(2022,1,1,0,0,0)
+timezone = pytz.timezone('Europe/Prague')
+timestamp_timescale = timezone.localize(timestamp_timescale)
 
-record = 'TEST_DATA,'
 
+with open('data_timescale.csv', 'a') as file:
+    file.write('Timestamp,D1,D2,D3,D4,Author,Value,Corrected_value' + '\n')
 
 for i in range(1000): 
     D1 = random_string_generator(4)
@@ -20,9 +27,21 @@ for i in range(1000):
     D4 = random_string_generator(4)
     AUTHOR = random_string_generator(10)
     timestamp = 1640991600
-    for j in range(8760):
+    for j in range(hours_in_year):
+       #random value generation
        VALUE = random.uniform(0,1000)
-       record = 'D1=' + D1 + ',' + 'D2=' + D2 + ',' + 'D3=' + D1 + ','+ 'D4=' + D4 + ' ' + 'AUTHOR=' + AUTHOR + ',' 'VALUE=' + str(VALUE) + ',' + 'CORRECTED_VALUE=' + str(round(VALUE, 2)) + ' ' + str(timestamp)
+       
+       #influx part
+       record_influx = 'TEST_DATA,' + 'D1=' + D1 + ',' + 'D2=' + D2 + ',' + 'D3=' + D3 + ','+ 'D4=' + D4 + ' ' + 'AUTHOR=' + AUTHOR + ',' 'VALUE=' + str(VALUE) + ',' + 'CORRECTED_VALUE=' + str(round(VALUE, 2)) + ' ' + str(timestamp)
        timestamp = timestamp + 3600
-       with open('soubor.txt', 'a') as soubor:
-            soubor.write(record + '\n')
+       
+       #timescale part
+       timestamp_str = timestamp_timescale.strftime("%Y-%m-%d %H:%M:%S.%f%z")
+       record_timescale = timestamp_str + ',' + D1 + ',' + D2 + ',' + D3 + ',' + D4 + ',' + AUTHOR + ',' + str(VALUE) + ',' + str(round(VALUE, 2))
+       timestamp_timescale += timedelta(hours=1)
+
+       #writing to files
+       with open('data_influx.csv', 'a') as file:
+            file.write(record_influx + '\n')
+       with open('data_timescale.csv', 'a') as file:
+           file.write(record_timescale + '\n')
